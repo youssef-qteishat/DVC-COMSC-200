985c7f9639be027866747f4d9b0da789803c52d4
yqteishat163 <54677265+yqteishat163@users.noreply.github.com>
2020-10-22 23:51:53 -0500

    Done

--- Youssef_Qteishat_200_assign7/Youssef_Qteishat_IntegerSet.h
+++ Youssef_Qteishat_200_assign7/Youssef_Qteishat_IntegerSet.h
@@ -1,4 +1,4 @@
-#ifndef YOUSSEF_QTEISHAT_INTEGERSET_H
+ifndef YOUSSEF_QTEISHAT_INTEGERSET_H
 #define YOUSSEF_QTEISHAT_INTEGERSET_H
 
 
@@ -9,21 +9,20 @@ using namespace std;
 
 class IntegerSet {
 public:
-	IntegerSet();
+	IntegerSet(); //1. first constructor works with sets that user inputs
+	IntegerSet(int[], int); // 2. second constructor that takes in array and its size to inilize the set object
 	
-	IntegerSet unionOfSets(IntegerSet); // joins two sets, given that there are no duplicates, and defines it as a new set
-	IntegerSet intersectionOfSets(IntegerSet); // finds where both values of A and B are true and returns it as a set set
-	void insertElement(); // inserts elemenet by setting index of element to true
-	void deleteElement(); // deletes element by setting index of element to false
+	void inputSet();
+	void insertElement(int); // 3. inserts elemenet by setting index of element to true
+	void deleteElement(int); // 4. deletes element by setting index of element to false
 	void printSet();
-	IntegerSet isEqualTo(IntegerSet); // determines whether to sets are equal. Used to create third theoretical set
 
-	IntegerSet(int [], int); // second constructor that takes in array and its size to inilize the set object
-	
+	IntegerSet unionOfSets(IntegerSet); // 5. joins two sets, given that there are no duplicates, and defines it as a new set
+	IntegerSet intersectionOfSets(IntegerSet); // 6. finds where both values of A and B are true and returns it as a set set
+	bool isEqualTo(IntegerSet); // 7. determines whether to sets are identical to oneanother
 
 private:
-	vector<bool> set; // 
-	int intArray[]; // array used to inilize set object
+	vector<bool> set; // 8. vector of bool value sthat will be inilized as false
 
 };
 #endif
