93cd274e479489e6861cce4e9672803d1a5f05a8
yqteishat163 <54677265+yqteishat163@users.noreply.github.com>
2020-10-22 23:52:45 -0500

    Done

--- Youssef_Qteishat_200_assign7/200_assign7.cpp
+++ Youssef_Qteishat_200_assign7/200_assign7.cpp
@@ -1,49 +1,54 @@
 #include <iostream> 
-#include "IntegerSet.h"
+#include "Youssef_Qteishat_IntegerSet.h"
 using namespace std;
 
 int main()
 {
-   IntegerSet a;
-   IntegerSet b;
-   IntegerSet c; // union of a and b
-   IntegerSet d; // intersection of a and b
+    IntegerSet a;
+    IntegerSet b;
+    IntegerSet c; // union of a and b
+    IntegerSet d; // intersection of a and b
 
-   cout << "Enter set A:\n";
-   a.inputSet();
-   cout << "\nEnter set B:\n";
-   b.inputSet();
-   c = a.unionOfSets( b );
-   d = a.intersectionOfSets( b );
-   cout << "\nUnion of A and B is:\n";
-   c.printSet();
-   cout << "Intersection of A and B is:\n";
-   d.printSet();
+    cout << "Enter set A:\n";
+    a.inputSet();
+    cout << "\nEnter set B:\n";
+    b.inputSet();
+    c = a.unionOfSets(b);
+    d = a.intersectionOfSets(b);
+    cout << "\nUnion of A and B is:\n";
+    c.printSet();
+    cout << "\nIntersection of A and B is:\n";
+    d.printSet();
 
-   if ( a.isEqualTo( b ) )
-      cout << "Set A is equal to set B\n";
-   else
-      cout << "Set A is not equal to set B\n";
+    if (a.isEqualTo(b) == true) {
+        
+        cout << "\nSet A is equal to set B\n";
+    }   
+    else if (a.isEqualTo(b) == false) {
 
-   cout << "\nInserting 77 into set A...\n";
-   a.insertElement( 77 );
-   cout << "Set A is now:\n";
-   a.printSet();
+        cout << "\nSet A is not equal to set B\n";
+    }
+        
 
-   cout << "\nDeleting 77 from set A...\n";
-   a.deleteElement( 77 );
-   cout << "Set A is now:\n";
-   a.printSet();
+    cout << "\nInserting 77 into set A...\n";
+    a.insertElement(77);
+    cout << "Set A is now:\n";
+    a.printSet();
 
-   const int arraySize = 10;
-   int intArray[ arraySize ] = { 25, 67, 2, 9, 99, 105, 45, -5, 100, 1 };
-   IntegerSet e( intArray, arraySize );
+    cout << "\nDeleting 77 from set A...\n";
+    a.deleteElement(77);
+    cout << "Set A is now:\n";
+    a.printSet();
 
-   cout << "\nSet E is:\n";
-   e.printSet();
+    const int arraySize = 10;
+    int intArray[arraySize] = { 25, 67, 2, 9, 99, 105, 45, -5, 100, 1 };
+    IntegerSet e(intArray, arraySize);
 
-   cout << endl;
+    cout << "\nSet E is:\n";
+    e.printSet();
 
-  //system("PAUSE");
-   return 0;
-} 
+    cout << endl;
+
+    //system("PAUSE");
+    return 0;
+}
