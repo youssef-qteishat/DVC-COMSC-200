3f236de73e4d7660f5d23733b63122d6ac47fdb6
yqteishat163 <54677265+yqteishat163@users.noreply.github.com>
2020-11-05 16:53:57 -0600

    Update Youssef_Qteishat_RationalNumber.h
    done

--- Youssef_Qteishat_200_Assign9/Youssef_Qteishat_RationalNumber.h
+++ Youssef_Qteishat_200_Assign9/Youssef_Qteishat_RationalNumber.h
@@ -8,34 +8,31 @@ using namespace std;
 class RationalNumber {
 
 public:
-	RationalNumber(int, int); /* constructor that takes in numerator and denominator and reduces fraction.
+	RationalNumber(int, int); /* #1 constructor that takes in numerator and denominator and reduces fraction.
 								it also prevents zero and negative values for denominator */
-	int getNumerator(); // returns numerator
-	int getDenominator(); // returns denominator
-	void printRational(); // prints reduced rational number.
+	int getNumerator(); // #2 returns numerator
+	int getDenominator(); // #3 returns denominator
+	void printRational(); // #4 prints reduced rational number.
 
-	// addition, subtraction, multiplication, and division operators will overloaded as friend fucntions with return type RationalNumber 
+	// #5 addition, subtraction, multiplication, and division operators will overloaded as member fucntions with return type RationalNumber 
 	RationalNumber operator+( RationalNumber); 
 	RationalNumber operator-(RationalNumber);
 	RationalNumber operator*(RationalNumber);
 	RationalNumber operator/(RationalNumber);
-	const RationalNumber operator=(const RationalNumber&);
 
-	// the relational operators will be overloaded as constant fucntions and will return a boolean value
+	// #6 similar process for overloaded relational operators, instead they return a boolean value
 	bool operator>(const RationalNumber&);
+	bool operator>=(const RationalNumber&);
+	bool operator<(const RationalNumber&);
+	bool operator<=(const RationalNumber&);
 
-	friend bool operator>=(const RationalNumber&, const RationalNumber&);
-	friend bool operator<(const RationalNumber&, const RationalNumber&);
-	friend bool operator<=(const RationalNumber&, const RationalNumber&);
-
-	// and the same applies to the equality and inequality operators
-	friend bool operator==(const RationalNumber&, const RationalNumber&);
-	friend bool operator!=(const RationalNumber&, const RationalNumber&);
-
+	// #7 same process for the overloaded equality and inequlity operators
+	bool operator==(const RationalNumber&);
+	bool operator!=(const RationalNumber&);
 	
 private:
-	int numerator, denominator; // parameters for constructor to create function
-	int getGCD(int, int); /* instead of calculating the GCD in the constructor,
-							were going to define a fucntion to find it instead */
+	int numerator, denominator; // #8 parameters for constructor to create function
+	int getGCD(int, int); /* #9 instead of finding the GCD in the constructor, we will define function to find it.
+						  this will make the code more concise*/
 };
 #endif
