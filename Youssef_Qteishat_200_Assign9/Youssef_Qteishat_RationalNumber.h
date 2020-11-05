b4eacbec734b3f68097fca5e4bd4302a84760287
yqteishat163 <54677265+yqteishat163@users.noreply.github.com>
2020-11-05 14:44:39 -0600

    Update Youssef_Qteishat_RationalNumber.h
    need to change relational overloaded operators to member functions. Otherwise, this is complete

--- Youssef_Qteishat_200_Assign9/Youssef_Qteishat_RationalNumber.h
+++ Youssef_Qteishat_200_Assign9/Youssef_Qteishat_RationalNumber.h
@@ -10,13 +10,32 @@ class RationalNumber {
 public:
 	RationalNumber(int, int); /* constructor that takes in numerator and denominator and reduces fraction.
 								it also prevents zero and negative values for denominator */
-	int getGCD(int, int); /* instead of calculating the GCD in the constructor, 
-							were going to define a fucntion to find it instead */
-	int getDenominator();
-	int getNumerator();
+	int getNumerator(); // returns numerator
+	int getDenominator(); // returns denominator
+	void printRational(); // prints reduced rational number.
 
+	// addition, subtraction, multiplication, and division operators will overloaded as friend fucntions with return type RationalNumber 
+	RationalNumber operator+( RationalNumber); 
+	RationalNumber operator-(RationalNumber);
+	RationalNumber operator*(RationalNumber);
+	RationalNumber operator/(RationalNumber);
+	const RationalNumber operator=(const RationalNumber&);
+
+	// the relational operators will be overloaded as constant fucntions and will return a boolean value
+	bool operator>(const RationalNumber&);
+
+	friend bool operator>=(const RationalNumber&, const RationalNumber&);
+	friend bool operator<(const RationalNumber&, const RationalNumber&);
+	friend bool operator<=(const RationalNumber&, const RationalNumber&);
+
+	// and the same applies to the equality and inequality operators
+	friend bool operator==(const RationalNumber&, const RationalNumber&);
+	friend bool operator!=(const RationalNumber&, const RationalNumber&);
+
+	
 private:
 	int numerator, denominator; // parameters for constructor to create function
-
+	int getGCD(int, int); /* instead of calculating the GCD in the constructor,
+							were going to define a fucntion to find it instead */
 };
 #endif
